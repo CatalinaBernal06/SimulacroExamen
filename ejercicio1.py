22c2f92075cff51b5c174096dca30efdec5b3ca1
catalinabernal06-simulacroexamen-qeru59w2 <catalinabernal06-simulacroexamen-qeru59w2@local>
2018-05-24 15:19:55 +0000

    simulacro

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,9 @@
 #Escriba el codigo necesario para que al ejecutar
 #python ejercicio1.py se impriman los enteros del 10 al 1 en cuenta regresiva.
+
+import numpy as np
+for i in range(10,0, -1):
+    print(i)
+    
+    
+    
--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,15 @@
 # Escriba el codigo necesario para que al ejecutar python ejercicio2.py
 # se cree una grafica con una funcion sinusoidal entre 0 y 2pi.
 
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 2*np.pi, 1000)
+y = np.sin(x)
+
+plt.figure()
+plt.plot(x, y, label = 'funcion sinusoidal')
+plt.legend()
+plt.savefig('sin.pdf')
